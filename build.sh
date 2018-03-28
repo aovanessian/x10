2af9af02df7e9e276de3a04e34b802d2fbe3b7cb
Ashot Ovanessian <ashot.ovanessian@ca.com>
2018-03-28 15:51:46 -0700

    More compile params

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ FILES=$(mktemp)
 find src -name *.java > $FILES
 cat $FILES
 mkdir -p bin
-javac -classpath lib/jSerialComm-1.3.11.jar -d bin -Xdiags:verbose @${FILES}
+javac -classpath lib/jSerialComm-1.3.11.jar -d bin -Xdiags:verbose -Xlint:unchecked @${FILES}
 rm $FILES
