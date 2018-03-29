37e5581dba566774564713811c125bd29e9168cf
Ashot Ovanessian <ashot.ovanessian@ca.com>
2018-03-28 21:25:05 -0700

    Removed unused fields

--- src/ca/tpmd/x10/Cmd.java
+++ src/ca/tpmd/x10/Cmd.java
@@ -2,35 +2,33 @@ package ca.tpmd.x10;
 
 enum Cmd
 {
-// 			addr	response length	label
-ALL_OFF			(false,	0,		"all units off"),
-LIGHTS_ON		(false,	0,		"all lights on"),
-ON			(true,	0,		"on"),
-OFF			(true,	0,		"off"),
-DIM			(true,	1,		"dim"),
-BRIGHT			(true,	1,		"brighten"),
-LIGHTS_OFF		(false,	0,		"all lights off"),
-EXT_CODE		(true,	2,		"extended code"),
-HAIL_REQ		(true,	0,		"hail request"),
-HAIL_ACK		(true,	0,		"hail acknowledge"),
-PRESET_DIM_1		(true,	0,		"pre-set dim 1"),
-PRESET_DIM_2		(true,	0,		"pre-set dim 2"),
-EXT_DATA_TFR		(true,	0,		"extended data transfer"),
-STATUS_ON		(true,	0,		"status on"),
-STATUS_OFF		(true,	0,		"status off"),
-STATUS_REQ		(true,	1,		"status request"),
-SYSTEM_STATE		(false,	0,		"system state"),
-EXIT			(false,	0,		"exit");
+// 			addr	label
+ALL_OFF			(false,	"all units off"),
+LIGHTS_ON		(false,	"all lights on"),
+ON			(true,	"on"),
+OFF			(true,	"off"),
+DIM			(true,	"dim"),
+BRIGHT			(true,	"brighten"),
+LIGHTS_OFF		(false,	"all lights off"),
+EXT_CODE		(true,	"extended code"),
+HAIL_REQ		(true,	"hail request"),
+HAIL_ACK		(true,	"hail acknowledge"),
+PRESET_DIM_1		(true,	"pre-set dim 1"),
+PRESET_DIM_2		(true,	"pre-set dim 2"),
+EXT_DATA_TFR		(true,	"extended data transfer"),
+STATUS_ON		(true,	"status on"),
+STATUS_OFF		(true,	"status off"),
+STATUS_REQ		(true,	"status request"),
+SYSTEM_STATE		(false,	"system state"),
+EXIT			(false,	"exit");
 
 private final boolean addr;
-private final int response_len;
 private final String label;
 private static final Cmd[] values = values();
 
-Cmd(boolean a, int res, String l)
+Cmd(boolean a, String l)
 {
 	addr = a;
-	response_len = res;
 	label = l;
 }
 
@@ -52,11 +50,6 @@ boolean need_addr()
 boolean need_dim()
 {
 	return this == DIM || this == BRIGHT;
-}
-
-int status_len()
-{
-	return response_len;
 }
 
 String label()
