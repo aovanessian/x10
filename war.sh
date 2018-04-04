cb739b128ab0a264cf43cdab794fa1f3aed86900
Ashot Ovanessian <ashot.ovanessian@ca.com>
2018-04-04 09:43:20 -0700

    war packaging script updates

--- war.sh
+++ war.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 FILES=$(mktemp)
-DIR=war/WEB-INF
+T=tmp
+W=$T/WEB-INF
 find src -name *.java > $FILES
 cat $FILES
-mkdir -p $DIR/classes $DIR/lib bin
-javac -classpath $CATALINA_HOME/lib/servlet-api.jar:lib/jSerialComm-1.3.11.jar -d $DIR/classes -Xdiags:verbose -Xlint:unchecked @${FILES}
-cp lib/* $DIR/lib
-cp res/web.xml $DIR
-cp -a res/html war
-cd war
-jar cf ../bin/x10.war .
-cd ..
-find war
-rm -rf war
-rm $FILES
+mkdir -p $W/classes $W/lib bin
+javac -classpath $CATALINA_HOME/lib/servlet-api.jar:lib/jSerialComm-1.3.11.jar -d $W/classes -Xdiags:verbose -Xlint:unchecked @${FILES}
+cp lib/* $W/lib
+cp res/web.xml $W
+cp -a res/html $T
+jar cf bin/x10.war -C $T .
+find $T
+rm -rf $T $FILES
