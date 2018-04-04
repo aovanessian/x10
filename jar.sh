500643e9dfb75116925a895dea686ed1e2c09499
Ashot Ovanessian <ashot.ovanessian@ca.com>
2018-04-04 09:22:23 -0700

    Packaging script updates

--- jar.sh
+++ jar.sh
@@ -3,14 +3,12 @@ FILES=$(mktemp)
 T=tmp
 find src -name *.java ! -path *http* > $FILES
 cat $FILES
+rm -rf $T
 mkdir -p $T bin
-rm -rf $T/*
 javac -classpath lib/jSerialComm-1.3.11.jar -d $T -Xdiags:verbose -Xlint:unchecked @${FILES}
-rm $FILES
-echo "Main-Class: ca.tpmd.x10.X10" > manifest
 cd $T
 jar xf ../lib/*jar
 rm -rf META-INF
-jar cfm ../bin/x10.jar ../manifest *
+jar cfe ../bin/x10.jar ca.tpmd.x10.X10 *
 cd ..
-rm -rf $T manifest
+rm -rf $T $FILES
