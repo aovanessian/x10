82ece1312746d77e22b44639519fee1661e3cab1
Ashot Ovanessian <ashot.ovanessian@ca.com>
2018-04-09 18:00:47 -0700

    Removing unneeded parameter, restricting access to package

--- src/ca/tpmd/x10/Cmd.java
+++ src/ca/tpmd/x10/Cmd.java
@@ -2,38 +2,36 @@ package ca.tpmd.x10;
 
 enum Cmd
 {
-// 			addr	label
-ALL_OFF			(false,	"all units off"),
-LIGHTS_ON		(false,	"all lights on"),
-ON			(false,	"on"),
-OFF			(false,	"off"),
-DIM			(false,	"dim"),
-BRIGHT			(false,	"brighten"),
-LIGHTS_OFF		(false,	"all lights off"),
-EXT_CODE_1		(true,	"extended code"),
-HAIL_REQ		(false,	"hail request"),
-HAIL_ACK		(false,	"hail acknowledge"),
-EXT_CODE_3		(true,	"extended code 3"),
-UNUSED			(false,	"unused"),
-EXT_CODE_2		(true,	"extended code 2"),
-STATUS_ON		(false,	"module on"),
-STATUS_OFF		(false,	"module off"),
-STATUS_REQ		(false,	"status request"),
-ADDRESS			(true,  "address unit(s)"),
-SYSTEM_STATE		(false,	"system state"),
-RING_ENABLE		(false, "enable serial ring"),
-RING_DISABLE		(false, "disable serial ring"),
-CLOCK_SET		(false, "set interface clock"),
-LOG_LEVEL		(false,	"log level"),
-EXIT			(false,	"exit");
 
-private final boolean addr;
+ALL_OFF			("all units off"),
+LIGHTS_ON		("all lights on"),
+ON			("on"),
+OFF			("off"),
+DIM			("dim"),
+BRIGHT			("brighten"),
+LIGHTS_OFF		("all lights off"),
+EXT_CODE_1		("extended code"),
+HAIL_REQ		("hail request"),
+HAIL_ACK		("hail acknowledge"),
+EXT_CODE_3		("extended code 3"),
+UNUSED			("unused"),
+EXT_CODE_2		("extended code 2"),
+STATUS_ON		("module on"),
+STATUS_OFF		("module off"),
+STATUS_REQ		("status request"),
+ADDRESS			( "address unit(s)"),
+SYSTEM_STATE		("system state"),
+RING_ENABLE		("enable serial ring"),
+RING_DISABLE		("disable serial ring"),
+CLOCK_SET		("set interface clock"),
+LOG_LEVEL		("log level"),
+EXIT			("exit");
+
 private final String label;
 private static final Cmd[] values = values();
 
-Cmd(boolean a, String l)
+Cmd(String l)
 {
-	addr = a;
 	label = l;
 }
 
@@ -49,7 +47,7 @@ boolean sys_cmd()
 
 boolean need_addr()
 {
-	return addr;
+	return this == ADDRESS;
 }
 
 boolean need_dim()
@@ -61,6 +59,5 @@ String label()
 {
 	return label;
 }
-
 
 }
